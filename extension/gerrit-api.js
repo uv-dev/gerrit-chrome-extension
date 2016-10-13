76fedef5065d853f087fd13d361d70d786bd75a5
LIMSUNG GUK <limasdf@gmail.com>
2016-10-13 23:00:39 +0900

    Style auto fix. there 're still style errors

--- extension/gerrit-api.js
+++ extension/gerrit-api.js
@@ -1,31 +1,31 @@
 function queryChangeList(q) {
-    var api_endpoint = localStorage["api_endpoint"];
-    var uname = localStorage["uname"];
-    var http_password = localStorage["http_password"];
-    chrome.webRequest.onAuthRequired.addListener(function (details) {
-        return {
-            authCredentials: {
-                username: uname,
-                password: http_password
+  var api_endpoint = localStorage['api_endpoint'];
+  var uname = localStorage['uname'];
+  var http_password = localStorage['http_password'];
+  chrome.webRequest.onAuthRequired.addListener(function(details) {
+      return {
+          authCredentials: {
+              username: uname,
+              password: http_password
             }
         };
     },
-    {urls: ["<all_urls>"]},
-    ['blocking']);
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", api_endpoint + '/changes/?q=' + q);
-        xhr.send();
-        // call to reject() is ignored once resolve() has been invoked
-        xhr.onload = function() {
-            try {
-                resolve(JSON.parse(xhr.responseText.substr(5)));
-            } catch (e) {
-                reject(new TypeError(e.message));
-            }
-        }
-        xhr.onloadend = function() {
-            reject(new Error("Network error"));
-        }
+  {urls: ['<all_urls>']},
+  ['blocking']);
+  return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', api_endpoint + '/changes/?q=' + q);
+      xhr.send();
+      // call to reject() is ignored once resolve() has been invoked
+      xhr.onload = function() {
+          try {
+            resolve(JSON.parse(xhr.responseText.substr(5)));
+          } catch (e) {
+            reject(new TypeError(e.message));
+          }
+        };
+      xhr.onloadend = function() {
+          reject(new Error('Network error'));
+        };
     });
 }
--- extension/popup.js
+++ extension/popup.js
@@ -1,10 +1,10 @@
-var BASE_URL = localStorage["api_endpoint"];
+var BASE_URL = localStorage['api_endpoint'];
 
 function getDelta(change) {
   var td = document.createElement('td');
   var ins = change.insertions ? change.insertions : 0;
   var del = change.deletions ? change.deletions : 0;
-  td.textContent = "+" + ins + ",-" + del;
+  td.textContent = '+' + ins + ',-' + del;
   return td;
 }
 
@@ -16,14 +16,14 @@ function getCodeReview(change) {
     : review.approved ? 2
     : 0;
   var td = document.createElement('td');
-  td.textContent = cr == 2 ? "+2"
-    : cr == 1 ? "+1"
-    : cr == 0 ? ""
-    : cr == -1 ? "-1"
-    : "-2";
-  td.style['color'] = cr > 0 ? "green"
-    : cr < 0 ? "red"
-    : "";
+  td.textContent = cr == 2 ? '+2'
+    : cr == 1 ? '+1'
+    : cr == 0 ? ''
+    : cr == -1 ? '-1'
+    : '-2';
+  td.style['color'] = cr > 0 ? 'green'
+    : cr < 0 ? 'red'
+    : '';
   return td;
 }
 
@@ -32,72 +32,72 @@ function getVerified(change) {
     : change.labels.Verified.rejeced ? -1
     : false;
   var td = document.createElement(td);
-  td.textContent = verified > 0 ? "+1"
-    : verified < 0 ? "-1"
-    : "";
-  td.style['color'] = verified > 0 ? "green"
-    : verified < 0 ? "red"
-    : "";
+  td.textContent = verified > 0 ? '+1'
+    : verified < 0 ? '-1'
+    : '';
+  td.style['color'] = verified > 0 ? 'green'
+    : verified < 0 ? 'red'
+    : '';
   return td;
 }
 
 function initUI(items) {
-    var list = document.getElementById('list_body');
-    list.innerHTML = "";
-    for (var i = 0; i < items.changes.length; i++) {
-        var change = items.changes[i];
-        if (change.reviewed)
-            continue;
+  var list = document.getElementById('list_body');
+  list.innerHTML = '';
+  for (var i = 0; i < items.changes.length; i++) {
+    var change = items.changes[i];
+    if (change.reviewed)
+        continue;
 
-        try {
-            change.read = new Date(items.timestamps[change._number]) >= new Date(change.updated);
-        } catch (e) {
-        }
-
-        var tr = document.createElement('tr');
-        var link = document.createElement('td');
-        var message = document.createElement('td');
-
-        var a = document.createElement('a');
-        a.href = "#";
-        a.addEventListener('click', function(e) {
-            chrome.tabs.update(null, {
-                url: BASE_URL + "/" + change._number
-            })
-        })
-
-        a.textContent = "cl/" + change._number;
-        link.appendChild(a);
-
-        message.textContent = change.subject;
-
-        tr.appendChild(link);
-        tr.appendChild(message);
-        tr.appendChild(getDelta(change));
-        tr.appendChild(getCodeReview(change));
-        tr.appendChild(getVerified(change));
-
-        if (change.read) {
-            tr.className = "read";
-        }
-
-        // TODO Use change.labels.Verified and change.labels.Code-Review here
-        // See https://gerrit.magicleap.com/Documentation/rest-api-changes.html#list-changes
-        list.appendChild(tr);
+    try {
+      change.read = new Date(items.timestamps[change._number]) >= new Date(change.updated);
+    } catch (e) {
     }
+
+    var tr = document.createElement('tr');
+    var link = document.createElement('td');
+    var message = document.createElement('td');
+
+    var a = document.createElement('a');
+    a.href = '#';
+    a.addEventListener('click', function(e) {
+        chrome.tabs.update(null, {
+            url: BASE_URL + '/' + change._number
+          });
+      });
+
+    a.textContent = 'cl/' + change._number;
+    link.appendChild(a);
+
+    message.textContent = change.subject;
+
+    tr.appendChild(link);
+    tr.appendChild(message);
+    tr.appendChild(getDelta(change));
+    tr.appendChild(getCodeReview(change));
+    tr.appendChild(getVerified(change));
+
+    if (change.read) {
+      tr.className = 'read';
+    }
+
+    // TODO Use change.labels.Verified and change.labels.Code-Review here
+    // See https://gerrit.magicleap.com/Documentation/rest-api-changes.html#list-changes
+    list.appendChild(tr);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    chrome.storage.local.get(["changes", "timestamps"], initUI);
-});
+    chrome.storage.local.get(['changes', 'timestamps'], initUI);
+  });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
     if (changes.changes && changes.timestamps) {
-        initUI({
-            "changes": changes.changes.newValue,
-            "timestamps": changes.timestamps.newValue
+      initUI({
+          'changes': changes.changes.newValue,
+          'timestamps': changes.timestamps.newValue
         });
     } else {
-        chrome.storage.local.get(["changes", "timestamps"], initUI);
+      chrome.storage.local.get(['changes', 'timestamps'], initUI);
     }
-});
+  });
