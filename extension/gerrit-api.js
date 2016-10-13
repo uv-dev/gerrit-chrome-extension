f995026493654f9507e4d70bd20fc02b6503644a
LIMSUNG GUK <limasdf@gmail.com>
2016-10-14 02:43:17 +0900

    Fix some jshint error in gerrit-api.js

--- extension/gerrit-api.js
+++ extension/gerrit-api.js
@@ -1,7 +1,7 @@
 function queryChangeList(q) {
-  var api_endpoint = localStorage['api_endpoint'];
-  var uname = localStorage['uname'];
-  var http_password = localStorage['http_password'];
+  var api_endpoint = localStorage.api_endpoint;
+  var uname = localStorage.uname;
+  var http_password = localStorage.http_password;
   chrome.webRequest.onAuthRequired.addListener(function(details) {
       return {
           authCredentials: {
