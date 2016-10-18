40bb147724fd1fafab39ee0df539d7dfdf1476fd
Sungguk Lim <limasdf@gmail.com>
2016-10-19 01:05:03 +0900

    Add more grunt tasks. (#19)
    * [WIP] add more dependencies for browserify
    
    * FIX
    
    * restruct
    
    * dist dir
    
    * Removing /dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,67 @@
 module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  // Configurable paths
+  var config = {
+    extension: 'extension',
+    dist: 'dist',
+    manifest: grunt.file.readJSON('extension/manifest.json')
+  };
+
   grunt.initConfig({
     pkg: '<json:package.json>',
+    config: config,
+    browserify: {
+      scripts:{
+        files:{
+          '<%= config.dist %>/scripts/popup.js': ['<%= config.extension %>/scripts/popup.js'],
+          '<%= config.dist %>/scripts/options.js': ['<%= config.extension %>/scripts/options.js'],
+          '<%= config.dist %>/scripts/background.js': ['<%= config.extension %>/scripts/background.js'],
+          '<%= config.dist %>/scripts/gerrit-api.js': ['<%= config.extension %>/scripts/gerrit-api.js']
+        }
+      }
+    },
+    clean: ['<%= config.dist %>/'],
+    // Copies remaining files to place other tasks can use.
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.extension %>',
+          dest: '<%= config.dist %>',
+          src: [
+            '{,*/}*.html',
+            'images/{,*/}*.png',
+            'manifest.json',
+            'styles/{,*/}*.css'
+          ]
+        }]
+      }
+    },
     jshint: {
       all: {
-        src: ['extension/*.js']
+        src: ['<%= config.extension %>/scripts/*.js']
       }
     },
     jscs: {
-      src: ['Gruntfile.js', 'extension/*.js'],
+      src: ['Gruntfile.js', '<%= config.extension %>/scripts/*.js'],
       options: {
         config: '.jscsrc',
         fix: true
       }
     },
     watch: {
-        files: ['extension/*.js'],
-        tasks: ['jshint', 'jscs']
-      }
+      files: ['<%= config.extension %>/scripts/*.js'],
+      tasks: ['jshint', 'jscs', 'browserify']
+    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jscs');
-
-  // Default task.
   grunt.registerTask('default', ['jshint', 'jscs', 'watch']);
 
-  // Test task
   grunt.registerTask('test', ['jshint', 'jscs']);
+
+  // TODO: Fix jshint errors and add `jshint` into build task.
+  grunt.registerTask('build', ['clean', 'browserify', 'copy']);
 };
